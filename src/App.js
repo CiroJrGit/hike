e8e84fc356db5a25db2e82b0695c0dce47f14d2e
Ciro Nogueira <cironogueira@edu.unirio.br>
2022-06-24 12:53:24 -0300

    Deployed project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
+import AuthProvider  from './contexts/auth';
+import { AppTheme } from './contexts/theme';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AppTheme>
+      <AuthProvider>
+         <BrowserRouter>
+            <GlobalStyle />
+
+            <Routes />
+         </BrowserRouter>
+      </AuthProvider>
+      </AppTheme>
   );
 }
 
