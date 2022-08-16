27f5588aab58b243d1123c1b9284917d47fc7b87
Ciro Nogueira <cironogueira@edu.unirio.br>
2022-08-15 22:06:15 -0300

    Entrega Final

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -2,20 +2,20 @@ const dark = {
    title: 'dark',
 
    colors: {
-      primary_font:          '#DBE0F1', //text
-      secondary_font:        '#6D779B', //placeholder 7c87ab
+      primary_font:          '#DBE0F1',
+      secondary_font:        '#6D779B', //7c87ab
       tertiary_font:         '#BBCAF3',
-      active_font:           '#9784ff', //text_hover
+      active_font:           '#9784ff',
 
-      primary:               '#100D31', //bg_component
-      secondary:             '#050322', //bg_root
+      primary:               '#100D31',
+      secondary:             '#050322',
 
-      primary_item:          '#1A173D', //bg_item
-      secondary_item:        '#323056', //bg_item_hover
+      primary_item:          '#1A173D',
+      secondary_item:        '#323056',
 
-      input_border:          '#404865', //input-border
+      input_border:          '#404865',
       toggle:                '#9784ff', 
-      error:                 '#B60000', //error
+      error:                 '#B60000',
 
       shadow:                'rgba(0, 0, 0, 0.6)',
       scrollbar:             'rgba(109, 119, 140, 0.34)',
